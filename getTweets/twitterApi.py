e2d473a2533dc1066191fbf19f43d8011ac19722
Geoff Langenderfer <geoff.langenderfer@gmail.com>
2021-04-05 04:25:22 -0400

    Update twitterApi.py

--- getTweets/twitterApi.py
+++ getTweets/twitterApi.py
@@ -2,10 +2,9 @@ import urllib.parse
 import base64
 
 fullSearchURI = "https://api.twitter.com/1.1/tweets/search/fullarchive/dev.json"
-#apiKey = "5q5C5EyDJju3GhZzhrhhRI0Ix"
-#apiKeySecret = "NYwRIl9S39r3ApCzIUxXlVtqdnisHP6EfViMPEjjHH5TLOs3Jj"
-apiKey = "NYwRIl9S39r3ApCzIUxXlVtqdnisHP6EfViMPEjjHH5TLOs3Jj"
-apiKeySecret = "NYwRIl9S39r3ApCzIUxXlVtqdnisHP6EfViMPEjjHH5TLOs3Jj"
+
+apiKey = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+apiKeySecret = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 
 def get_bearer_token():
   bearer_token_credentials = get_bearer_token_credentials(apiKey, apiKeySecret)
@@ -19,4 +18,4 @@ def get_bearer_token(credentials):
     pass
 
 creds = get_bearer_token_credentials(apiKey, apiKeySecret)
-print(creds) #== "eHZ6MWV2RlM0d0VFUFRHRUZQSEJvZzpMOHFxOVBaeVJnNmllS0dFS2hab2xHQzB2SldMdzhpRUo4OERSZHlPZw==")
+print(creds) #== "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
